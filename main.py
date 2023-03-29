69404dde71b64e5cd3a4554700febfd2d4b3c589
YJGS-dev <yordi-futbol@hotmail.com>
2023-03-29 11:24:48 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI, Path
+from scrap import (
+    getWalmartHTML,
+    getHTMLParser,
+    getScrapWalmartCategoriesDespensa,
+    getDriver,
+    getTiendasJumboHTML,
+    getHostWebSite,
+    getScrapProductsTiendasJumbo
+)
+
+app = FastAPI()
+
+@app.get("/walmart")
+async def walmart():
+    html = getWalmartHTML()
+    soup_object = getHTMLParser(html)
+    data = {
+        "department": "Despensa",
+        "url": "https://super.walmart.com.mx/",
+        "categories": []
+    }
+    categories = getScrapWalmartCategoriesDespensa(soup_object)
+    data["categories"] = categories
+    return data
+
+
+@app.get("/tiendas_jumbo/{url:path}")
+async def tiendas_jumbo(url: str = Path(..., description="URL de la categoria del sitio")):
+    url = url
+    browser = getDriver()
+    html = getTiendasJumboHTML(url, browser)
+    website_host = getHostWebSite(browser)
+    browser.close()
+    data = {
+        "url": website_host,
+        "products": []
+    }
+    soup_object = getHTMLParser(html)
+    products = getScrapProductsTiendasJumbo(soup_object, website_host)
+    data["products"] = products
+    return data
